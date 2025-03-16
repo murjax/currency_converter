6235419b48480d0521afe0a27be20763f54d5f6a
Ryan Murphy <murjax@gmail.com>
2025-03-16 11:57:03 -0400

    Dry up the spec

--- spec/currency_converter_spec.rb
+++ spec/currency_converter_spec.rb
@@ -2,86 +2,91 @@ require_relative '../currency_converter'
 
 RSpec.describe CurrencyConverter do
   describe '#format' do
-    it 'formats USD' do
-      usd_amount = 1225.25
-      result = described_class.new(usd_amount, 'USD').format
-      expect(result).to eq('$1,225.25')
+    let(:usd_amount) { 1225.25 }
+    let(:currency) { 'USD' }
+
+    subject { described_class.new(usd_amount, currency).format }
+
+    context 'formats USD' do
+      it { is_expected.to eq('$1,225.25') }
     end
 
-    it 'converts and formats USD to GBP' do
-      usd_amount = 1225.25
-      result = described_class.new(usd_amount, 'GBP').format
-      expect(result).to eq('£950.43')
+    context 'convert and format USD to GBP' do
+      let(:currency) { 'GBP' }
+
+      it { is_expected.to eq('£950.43') }
     end
 
-    it 'converts and formats USD to EUR' do
-      usd_amount = 1225.25
-      result = described_class.new(usd_amount, 'EUR').format
-      expect(result).to eq('€1.133,85')
+    context 'convert and format USD to EUR' do
+      let(:currency) { 'EUR' }
+
+      it { is_expected.to eq('€1.133,85') }
     end
 
-    it 'supports string inputs' do
-      usd_amount = '1225.25'
-      result = described_class.new(usd_amount, 'USD').format
-      expect(result).to eq('$1,225.25')
+    context 'support string inputs' do
+      let(:usd_amount) { '1225.25' }
+
+      it { is_expected.to eq('$1,225.25') }
     end
 
-    it 'supports string inputs' do
-      usd_amount = '1225.25'
-      result = described_class.new(usd_amount, 'USD').format
-      expect(result).to eq('$1,225.25')
+    context 'pad zeros on fractional cents' do
+      let(:usd_amount) { 1225 }
+
+      it { is_expected.to eq('$1,225.00') }
     end
 
-    it 'pads zeros on fractional cents' do
-      usd_amount = 1225
-      result = described_class.new(usd_amount, 'USD').format
-      expect(result).to eq('$1,225.00')
+    context 'return nil when currency unsupported' do
+      let(:currency) { 'FOO' }
+
+      it { is_expected.to be_nil }
     end
 
-    it 'returns nil if currency unsupported' do
-      usd_amount = 1225.25
-      result = described_class.new(usd_amount, 'FOO').format
-      expect(result).to be_nil
-    end
+    context 'return nil when amount is non-numeric' do
+      let(:usd_amount) { 'foo' }
 
-    it 'returns nil if amount is non-numeric' do
-      usd_amount = 'foo'
-      result = described_class.new(usd_amount, 'USD').format
-      expect(result).to be_nil
+      it { is_expected.to be_nil }
     end
   end
 
   describe '.format' do
-    it 'is same as #format' do
-      usd_amount = 1225.25
-      result = described_class.format(usd_amount, 'USD')
-      expect(result).to eq('$1,225.25')
+    subject { described_class.format(usd_amount, currency) }
+
+    context 'point to #format' do
+      let(:usd_amount) { 1225.25 }
+      let(:currency) { 'USD' }
+
+      it { is_expected.to eq('$1,225.25') }
     end
   end
 
   describe '.add' do
-    it 'adds amounts and returns formatted total for given currency' do
-      amounts = [100.25, 500.50, 900.90]
-      result = described_class.add(amounts, 'EUR')
-      expect(result).to eq('€1.389,63')
+    let(:amounts) { [100.25, 500.50, 900.90] }
+    let(:currency) { 'USD' }
+
+    subject { described_class.add(amounts, currency) }
+
+    context 'add amounts and return formatted total for given currency' do
+      let(:currency) { 'EUR' }
+
+      it { is_expected.to eq('€1.389,63') }
     end
 
-    it 'returns nil if currency unsupported' do
-      amounts = [100.25, 500.50, 900.90]
-      result = described_class.add(amounts, 'FOO')
-      expect(result).to be_nil
+    context 'return nil when currency unsupported' do
+      let(:currency) { 'FOO' }
+
+      it { is_expected.to be_nil }
     end
 
-    it 'returns nil if any amounts are non-numeric' do
-      amounts = [100.25, 'foo', 900.90]
-      result = described_class.add(amounts, 'EUR')
-      expect(result).to be_nil
+    context 'return nil when any amounts are non-numeric' do
+      let(:amounts) { [100.25, 'foo', 900.90] }
+
+      it { is_expected.to be_nil }
     end
 
-    it 'supports large floating point entries' do
-      amounts = [0.1, 0.05, 1000000000000000.0]
-      result = described_class.add(amounts, 'USD')
-      expect(result).to eq('$1,000,000,000,000,000.15')
+    context 'support large floating point entries' do
+      let(:amounts) { [0.1, 0.05, 1000000000000000.0] }
+
+      it { is_expected.to eq('$1,000,000,000,000,000.15') }
     end
   end
 end
