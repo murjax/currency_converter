45475f3143cb39e2f731cb387b03bfa3085f2ebf
michaeldimmitt <michaelgdimmitt@gmail.com>
2025-03-21 06:43:58 -0400

    all tests passing, move stateless helper functions outside of class

--- currency_converter.rb
+++ currency_converter.rb
@@ -22,14 +22,8 @@ class CurrencyConverter
     @currency = currency;
   end
 
-  def self.chart()
-    # https://hackernoon.com/ruby-how-to-readwrite-json-file-a23h3vxa
-    _file = File.read('./conversion_chart.json')
-    @data_hash = JSON.parse(_file)
-  end
-
   def format()
-    self.class.doFormatting(@amount, @currency)
+    doFormatting(@amount, @currency)
   end
 
   def self.format(amount, currency)
@@ -45,44 +39,47 @@ class CurrencyConverter
     _currency = currency;
     doFormatting(_amount, _currency)
   end
+end
 
+def chart()
+  # https://hackernoon.com/ruby-how-to-readwrite-json-file-a23h3vxa
+  _file = File.read('./conversion_chart.json')
+  @data_hash = JSON.parse(_file)
+end
 
+def doFormatting(amount, currency)
+  begin
+  ## string interpolation to show the currency presented.
+  ## test will fail because no comma.
+  # "$#{@amount}"
 
-  def self.doFormatting(amount, currency)
-    begin
-    ## string interpolation to show the currency presented.
-    ## test will fail because no comma.
-    # "$#{@amount}"
+  ## Bring in money library
 
-    ## Bring in money library
+  ## Regarding Variables in Ruby
+  # +------------------+----------------------+
+  # | Name Begins With |    Variable Scope    |
+  # +------------------+----------------------+
+  # | $                | A global variable    |
+  # | @                | An instance variable |
+  # | [a-z] or _       | A local variable     |
+  # | [A-Z]            | A constant           |
+  # | @@               | A class variable     |
+  # +------------------+----------------------+
 
-    ## Regarding Variables in Ruby
-    # +------------------+----------------------+
-    # | Name Begins With |    Variable Scope    |
-    # +------------------+----------------------+
-    # | $                | A global variable    |
-    # | @                | An instance variable |
-    # | [a-z] or _       | A local variable     |
-    # | [A-Z]            | A constant           |
-    # | @@               | A class variable     |
-    # +------------------+----------------------+
+  ## We are always using usd amount.
+  return nil if(amount.nil?)
+  _amount = Money.from_cents(amount, 'USD')
+  _amount.format()
 
-    ## We are always using usd amount.
-    return nil if(amount.nil?)
-    _amount = Money.from_cents(amount, 'USD')
-    _amount.format()
+  ## We may or may not convert to another amount.
+  ## Symmetric Properties - if a usd amount is converted to usd the value should not change.
 
-    ## We may or may not convert to another amount.
-    ## Symmetric Properties - if a usd amount is converted to usd the value should not change.
+  chart
+  _val = @data_hash[currency]
 
-    chart
-    _val = @data_hash[currency]
-
-    Money.default_bank.add_rate('USD', currency, _val)
-    _finalAmount = _amount.exchange_to(currency).format
-    rescue
-      nil
-    end
+  Money.default_bank.add_rate('USD', currency, _val)
+  _finalAmount = _amount.exchange_to(currency).format
+  rescue
+    nil
   end
-
 end
