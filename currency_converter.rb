5c57e8d83a658d051984928e167a90f4b4950b1e
michaeldimmitt <michaelgdimmitt@gmail.com>
2025-03-20 21:28:16 -0400

    5 tests passing, I dont like it but format class method has been added and class variables are being used.

--- currency_converter.rb
+++ currency_converter.rb
@@ -15,22 +15,28 @@ class CurrencyConverter
   ## 1. allow the new command to be invoked for this class by adding an initialize function.
   def initialize(amount, currency)
     ## 2. set class scoped variables.
-    @amount = amount;
-    @currency = currency;
+    @@currency = currency;
+    @@amount = amount;
     # puts(amount, currency)
   end
 
-  def chart()
+  def self.chart()
     # https://hackernoon.com/ruby-how-to-readwrite-json-file-a23h3vxa
     _file = File.read('./conversion_chart.json')
     @data_hash = JSON.parse(_file)
   end
 
   def format()
+    self.class.doFormatting
+  end
+
+  def self.format(amount, currency)
+    @@currency = currency;
+    @@amount = amount;
     doFormatting
   end
 
-  def doFormatting()
+  def self.doFormatting()
     ## string interpolation to show the currency presented.
     ## test will fail because no comma.
     # "$#{@amount}"
@@ -49,20 +55,17 @@ class CurrencyConverter
     # +------------------+----------------------+
 
     ## We are always using usd amount.
-    _amount = Money.from_amount(@amount, 'USD')
+    _amount = Money.from_amount(@@amount, 'USD')
     _amount.format()
 
     ## We may or may not convert to another amount.
     ## Symmetric Properties - if a usd amount is converted to usd the value should not change.
 
-    # @data_hash[@currency]
-    chart()
-    _val = @data_hash[@currency]
-    # puts 'hi'
-    # puts _val
-    # puts 'bye'
-    Money.default_bank.add_rate('USD', @currency, _val)
-    _finalAmount = _amount.exchange_to(@currency).format
+    chart
+    _val = @data_hash[@@currency]
+
+    Money.default_bank.add_rate('USD', @@currency, _val)
+    _finalAmount = _amount.exchange_to(@@currency).format
   end
 
 end
